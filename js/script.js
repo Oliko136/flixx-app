f230207e8c16d6fb3b60ce2d99a9501e1fc7b244
Oliko136 <olchick136@gmail.com>
2025-09-08 11:10:22 +0400

    Display popular TV shows

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
     currentPage: window.location.pathname
 };
 
+// Display 20 most popular movies
 async function displayPopularMovies() {
     const { results } = await fetchAPIData('movie/popular');
 
@@ -32,6 +33,40 @@ async function displayPopularMovies() {
             </div>`
         
         document.querySelector('#popular-movies').appendChild(div);
+    })
+}
+
+// Display 20 most popular TV shows
+async function displayPopularShows() {
+    const { results } = await fetchAPIData('tv/popular');
+
+    results.forEach(show => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML =
+            `<a href="tv-details.html?id=${show.id}">
+            ${
+        show.poster_path
+            ? `<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+            />`
+            : `<img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${show.name}</h5>
+                <p class="card-text">
+                <small class="text-muted">Aired: ${show.first_air_date}</small>
+                </p>
+            </div>`
+        
+        document.querySelector('#popular-shows').appendChild(div);
     })
 }
 
@@ -77,7 +112,7 @@ function init() {
             displayPopularMovies();
             break;
         case '/shows.html':
-            console.log('Shows');
+            displayPopularShows();
             break;
         case '/movie-details.html':
             console.log('Movie Details');
